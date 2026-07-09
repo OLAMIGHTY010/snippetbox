43d29017718c7ff45fd4f930a57edb74c644e846
oolatunbo <Nanicomlekan111@gmail.com>
2026-07-09 16:36:07 +0100

    updating snipettview status method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request){
@@ -14,19 +16,19 @@ func home(w http.ResponseWriter, r *http.Request){
 
 }
 func snippetview(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodPost{
-		w.Header().Set("Allow","POST")
-		http.Error(w, "method not allowed ", 405)
+	id,err :=strconv.Atoi(r.URL.Query().Get("id"))
+	if err !=nil || id < 1{
+		http.NotFound(w,r)
 		return
 	}
-	w.Write([]byte("this is the snippetview"))
+	fmt.Fprintf(w,"Display snippett view ID %d",id)
 
 }
 
 func snippetcreate(w http.ResponseWriter, r *http.Request){
 	if r.Method != http.MethodPost{
-		w.Header().Set("Allow","POST")
-		http.Error(w, "method not allowed", 405)
+		w.Header().Set("Allow",http.MethodPost)
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("This creating a snippet view "))
